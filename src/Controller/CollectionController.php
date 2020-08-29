680082a930f6c52fb3ef565f948fe81f95504fb2
ilincaav <ilinca_vultur17@yahoo.com>
2020-08-29 16:16:25 +0200

    Added: Collection of Adventures

---
+++ src/Controller/CollectionController.php
@@ -0,0 +1,128 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\Adventure;
+use App\Form\AdventureType;
+use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * Class CollectionController
+ * @package App\Controller
+ * @Route("/AdventureOfALifetime/Collection")
+ */
+class CollectionController extends AbstractController
+{
+    /**
+     * @param EntityManagerInterface $em
+     * @param Request $request
+     * @return RedirectResponse|Response
+     * @Route("/addAdventure", name="app_new_adventure")
+     */
+    public function addAdventure(EntityManagerInterface $em, Request $request)
+    {
+        $form = $this->createForm(AdventureType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $em->persist($form->getData());
+
+            $em->flush();
+            return $this->redirect($this->generateUrl('app_collection'));
+        }
+        return $this->render(
+            'Collection/addAdventure.html.twig',
+            [
+                'user_form' => $form->createView()
+            ]
+        );
+
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/list", name="app_collection")
+     */
+    public function listAction(Request $request)
+    {
+        $productRepository = $this->getDoctrine()->getRepository(Adventure::class);
+        $qb = $productRepository->findAdventures($request->get('search'));
+
+        $page = $request->get('page');
+        $pager = new Pagerfanta(new DoctrineORMAdapter($qb));
+        $pager->setCurrentPage($page?$page:1);
+        $pager->getNbResults();
+
+        return $this->render(
+            'Collection/list.html.twig',
+            [
+                'pager' => $pager
+            ]
+        );
+
+    }
+
+    /**
+     * @param Adventure $adventure
+     * @return RedirectResponse
+     * @Route("/delete/{id}", name="app_adventure_delete")
+     */
+    public function adventureDelete(Adventure $adventure)
+    {
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($adventure);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_collection');
+
+    }
+
+    /**
+     * @param Request $request
+     * @param Adventure $adventure
+     * @return RedirectResponse|Response
+     * @Route("/edit/{id}", name="app_adventure_edit")
+     */
+    public function update(Request $request, Adventure $adventure)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+
+        $form = $this->createForm(AdventureType::class, $adventure);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_collection');
+        }
+
+        return $this->render(
+            'Collection/editAdventure.html.twig',
+            [
+                'user_form' => $form->createView()
+            ]
+        );
+    }
+
+
+
+
+
+
+}
---
+++ src/Form/AdventureType.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Form;
+
+
+use App\Entity\Adventure;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdventureType extends AbstractType
+{
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Location', TextType::class)
+            ->add('Date', \DateTime::class)
+            ->add('Type', ChoiceType::class, [
+                'choices' => [
+                    'Hiking' => Adventure::TYPE_HIKING ,
+                    'Skiing' => Adventure::TYPE_SKIING,
+                    'Surfing' => Adventure::TYPE_SURFING ,
+                    'Swimming' => Adventure::TYPE_SWIMMING ,
+                    'Snowboarding' => Adventure::TYPE_SNOWBOARDING,
+                    'Riding' => Adventure::TYPE_RIDING ,
+                    'Sailing' => Adventure::TYPE_SAILING ,
+                ],
+
+            ])
+        ;
+
+
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Adventure::class
+        ]);
+    }
+
+
+
+}
